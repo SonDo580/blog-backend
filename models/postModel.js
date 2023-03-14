3afb6fa947e420606a6556bbffe1c4a885a4c0e3
Son Do <dominhson580@gmail.com>
2023-03-14 22:26:43 +0700

    Add more fields to post

--- models/postModel.js
+++ models/postModel.js
@@ -3,12 +3,18 @@ const mongoose = require("mongoose");
 const postSchema = mongoose.Schema(
   {
     title: { type: String, required: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Category",
+    },
     content: { type: String, required: true },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
     },
+    published: false,
   },
   { timestamps: true }
 );

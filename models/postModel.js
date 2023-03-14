424dcbfc034e99cd8435d06bf2408d999fc35abe
Son Do <dominhson580@gmail.com>
2023-03-14 08:28:17 +0700

    Create model Post

---
+++ models/postModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);

8d2f36348c8ec5e9dfa9c20b19f5ea2438623ca4
Son Do <dominhson580@gmail.com>
2023-03-15 08:19:10 +0700

    Add field 'isEditor'

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isEditor: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("User", userSchema);
